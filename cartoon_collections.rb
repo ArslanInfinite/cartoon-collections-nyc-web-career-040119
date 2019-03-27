4487f7ceb8fc528a49d15cf928cd9fd5b654b6ff
Arslan Ali <ali.arslan319@gmail.com>
2019-03-27 21:24:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,22 +4,11 @@ def roll_call_dwarves(the_dwarves)
   end
 end
 
-# def summon_captain_planet# code an argument here
-#   # Your code here
-# end
-
-planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
-
 def summon_captain_planet(array)
   array.collect do |word|
     word.capitalize + "!"
   end
 end
-
-summon_captain_planet(planeteer_calls)
-
-short_words = ["puff", "go", "two"]
-assorted_words = ["two", "go", "industrious", "bop"]
 
 def long_planeteer_calls(array)
   array.any? do |word|
@@ -27,51 +16,8 @@ def long_planeteer_calls(array)
   end
 end
 
-long_planeteer_calls(short_words)
-long_planeteer_calls(assorted_words)
 
-# describe "#summon_captain_planet" do
-#   it "returns an array with the same number of elements that it was given" do
-#     veggies = ["carrot", "cucumber", "pepper"]
-#     result = summon_captain_planet(veggies)
-#     expect(result.class).to eq(Array)
-#     expect(result.length).to eq(3)
-#   end
-#
-#   it "capitalizes each element and adds an exclamation mark (test 1)" do
-#     fruits = ["apple", "banana", "orange"]
-#     result = summon_captain_planet(fruits)
-#     ['Apple!', 'Banana!', 'Orange!'].each { |w| expect(result).to include w }
-#   end
-#
-#   it "capitalizes each element and adds an exclamation mark (test 2)" do
-#     veggies = ["carrot", "cucumber", "pepper"]
-#     result = summon_captain_planet(veggies)
-#     ["Carrot!", "Cucumber!", "Pepper!"].each { |w| expect(result).to include w }
-#   end
-# end
-#
-# describe "#long_planeteer_calls" do
-#   it "returns true if any calls are longer than 4 characters" do
-#     calls_long = ["axe", "earth", "wind", "fire"]
-#     expect(long_planeteer_calls(calls_long)).to eq(true)
-#   end
-#
-#   it "returns false if all calls are shorter than 4 characters" do
-#     calls_short = ["wind", "fire", "tree", "axe", "code"]
-#     expect(long_planeteer_calls(calls_short)).to eq(false)
-#   end
-#
-#
-#
-#
-#
-#
-# #
-# # def long_planeteer_calls# code an argument here
-# #   # Your code here
-# # end
-# #
+
 # # def find_the_cheese# code an argument here
 # #   # the array below is here to help
 # #   cheese_types = ["cheddar", "gouda", "camembert"]
