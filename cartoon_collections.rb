c7a6d7a6a88e0e8c91d9edc567bc0fbdc9586561
Arslan Ali <ali.arslan319@gmail.com>
2019-03-27 21:26:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,14 +4,14 @@ def roll_call_dwarves(the_dwarves)
   end
 end
 
-def summon_captain_planet(array)
-  array.collect do |word|
+def summon_captain_planet(edible)
+  edible.collect do |word|
     word.capitalize + "!"
   end
 end
 
-def long_planeteer_calls(array)
-  array.any? do |word|
+def long_planeteer_calls(long_words)
+  long_words.any? do |word|
     word.length > 4
   end
 end
